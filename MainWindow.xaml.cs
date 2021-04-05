8a10fdeba9eafcbbc805ed4aa1581ec454c16340
Artem <alexey.artem.alexey.artem@gmail.com>
2021-04-06 00:01:38 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalculationNumericalSeries
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        NumericalSeries numericalSeries;
+        public MainWindow()
+        {
+            InitializeComponent();
+            numericalSeries = new NumericalSeries("2*n");
+            LabelResult.Content += numericalSeries.GetSum(1, 3).ToString();
+        }
+    }
+}
---
+++ NumericalSeries.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Symbolics;
+
+namespace CalculationNumericalSeries
+{
+    class NumericalSeries
+    {
+        private Dictionary<string, FloatingPoint> variable;
+        private SymbolicExpression funcExpression;
+
+        public NumericalSeries(string function) 
+        {
+            funcExpression = SymbolicExpression.Parse(function);
+            variable = new Dictionary<string, FloatingPoint>();
+            variable.Add("n", 0);
+        }
+
+        public double GetSum(int startNumber, int endNumber)
+        {
+            double sum = 0;
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                variable["n"] = i;
+                sum += funcExpression.Evaluate(variable).RealValue;
+            }
+
+            return sum;
+        }
+    }
+
+}
